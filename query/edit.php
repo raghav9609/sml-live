a48d679c2ed06365f35e170dd04912517e16a04e
Sumit Raghav <raghav9609@gmail.com>
2024-02-21 13:56:14 +0530

    as

--- query/edit.php
+++ query/edit.php
@@ -47,7 +47,7 @@ if ($exe_form['id'] == '' || $exe_form['id'] == 0) {
     $description = $exe_form['description'];
     $user_name = $exe_form['user_name'];
     $contact_no = $exe_form['contact_no'];
-    $loantype_name = get_name('master_code_id',$exe_form['loan_type']);
+    $loantype_name = get_name('master_code_id',$exe_form['loan_type'])["value"];
     $lead_date_time = $exe_form['date'];
     $new_src = str_replace('', '', $exe_form['page_url']);
     $src_exp = explode("/", $new_src);
@@ -82,6 +82,8 @@ if ($exe_form['id'] == '' || $exe_form['id'] == 0) {
     }
     $pan_card = $result_cust_data['pan_card'];
     $city_id = $result_cust_data['city_id'];
+    $getcityId = get_name("city_id", $city_id);
+    $city_name = getcityId["city_name"];
     $alt_phone = $result_cust_data['alt_phone'];
     $account_no = $result_cust_data['account_no'];
     $office_landline = $result_cust_data['ofc_contact'];
@@ -90,7 +92,8 @@ if ($exe_form['id'] == '' || $exe_form['id'] == 0) {
     $ofc_email = $result_cust_data['ofc_email'];
     $work_city = $result_cust_data['work_city'];
     $maritalstatus = $result_cust_data['maritalstatus'];
-    $ofc_city_name = get_display_name("city_name", $work_city);
+    $getofccityId = get_name("city_id", $work_city);
+    $ofc_city_name = getofccityId["city_name"];
     $curDate = currentDate();
     $result_cust_data['cur_comp_wrk_exp'];
     $ccweget = dateDiff($curDate,$result_cust_data['cur_comp_wrk_exp'],1);
--- query/form_index.php
+++ query/form_index.php
@@ -44,7 +44,7 @@
    
                 $nametext = trim($fname) != '' ? "<li><b class='fw_bold'>".ucfirst($fname)."</b> " : '<li><b class="fw_bold">Customer</b> '; 
                 $dobtext = ($diff != '') ? " and customer age is&nbsp;&nbsp;<b class='fw_bold'>".$diff."</b>&nbsp;&nbsp;years </li>" : "</li>"  ;
-                $citytext = (($city_name != '')? " and residing in <b class='fw_bold'> $city_name </b></li> " : "</li>") ;
+                $citytext = (($city_name != '') ? " and residing in <b class='fw_bold'> $city_name </b></li> " : "</li>") ;
                 $occcc = get_name('master_code_id',$occup);
     
                 $occuptext = (($occup != '' && $occup != 0) ? "<li>Customer is  <b class='fw_bold'>".$occcc['value']." </b>" : "");
