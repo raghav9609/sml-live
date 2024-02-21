ced68f276c8a7fe6662a08d2f2d3a28d8e01e384
Sumit Raghav <raghav9609@gmail.com>
2024-02-21 18:17:41 +0530

    chnges

--- sms/send-sms-process.php
+++ sms/send-sms-process.php
@@ -8,7 +8,7 @@ $subject = $_REQUEST['subject'];
 $description = trim($_REQUEST['description']);
 $temp_id = $_REQUEST['template'];											
 $query_id = $_REQUEST['query_id']; 
-echo $url= "https://bulkpush.mytoday.com/BulkSms/SingleMsgApi?feedid=385302&username=9920706289&password=SML2021@123&senderid=SML&To=91".$subject."&Text=".$description;
+echo $url= "https://bulkpush.mytoday.com/BulkSms/SingleMsgApi?feedid=385302&username=9920706289&password=SML2021@123&senderid=SML&To=91".$subject."&Text=Congratulations DURWANG UDAY SAWANT! Your loan has been disbursed by bank. Expect the amount in your account within 24 working hours. Any questions? Contact us at Bharat Bhushan. We'd love to hear about your experience! Share your thoughts/Experince at - https://www.switchmyloan.in/testimonials. Cheers to financial success.";
 $response = curl_get_helper(trim($url));
 
 print_r($response);
