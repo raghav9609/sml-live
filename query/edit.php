fb7058375cdefd349a89025c7a9f53b1573ce6f6
Sumit Raghav <raghav9609@gmail.com>
2024-02-21 19:09:16 +0530

    as

--- query/edit.php
+++ query/edit.php
@@ -418,7 +418,7 @@ if ($exe_form['id'] == '' || $exe_form['id'] == 0) {
                                         $.ajax({
                                             type: "POST",
                                             dataType: "text",
-                                            data: "type="+type+"&query_id="+query_id+"&otp="+result.value+"&stgOneHitId="+obj.stgOneHitId+"&stgTwoHitId="+obj.stgTwoHitId,
+                                            data: "type="+type+"&query_id="+query_id+"&otp="+btoa(result.value)+"&stgOneHitId="+btoa(obj.stgOneHitId)+"&stgTwoHitId="+btoa(obj.stgTwoHitId),
                                             cache: false,
                                             url: "<?php echo $head_url;?>/experian/validate-otp.php",
                                             success: function (data) {
