664d394d421015c398bb6a91f0352ea85e96226d
Sumit Raghav <raghav9609@gmail.com>
2024-02-21 18:14:52 +0530

    changea

--- sms/send-sms-process.php
+++ sms/send-sms-process.php
@@ -8,8 +8,8 @@ $subject = $_REQUEST['subject'];
 $description = trim($_REQUEST['description']);
 $temp_id = $_REQUEST['template'];											
 $query_id = $_REQUEST['query_id']; 
-echo $url= "https://bulkpush.mytoday.com/BulkSms/SingleMsgApi?feedid=385302&username=9920706289&password=SML2021@123&senderid=SML&To=91".$subject."&Text=".$description."&async=1";
-$response = curl_get_helper($url);
+echo $url= "https://bulkpush.mytoday.com/BulkSms/SingleMsgApi?feedid=385302&username=9920706289&password=SML2021@123&senderid=SML&To=91".$subject."&Text=".$description.;
+$response = curl_get_helper(rtrim($url));
 
 print_r($response);
 $qry = "INSERT INTO crm_communication_history SET query_id='".$query_id."',type = 2, communication_id = '".$subject."',response='".base64_encode($response)."',description = '".base64_encode($description)."'";
