c8985dad19f313643e5fe7d8847c0d094ca3e406
Sumit Raghav <raghav9609@gmail.com>
2024-02-21 17:15:10 +0530

    changes

--- experian/index.php
+++ experian/index.php
@@ -24,14 +24,39 @@ if($type == 1){
     $url = "https://ecvuat.experian.in/ECV-P2/content/registerEnhancedMatchMobileOTP.action";
     echo $content = 'clientName=SWITCH_EM&allowInput=1&allowEdit=1&allowCaptcha=1&allowConsent=1&allowEmailVerify=1&allowVoucher=1&voucherCode=SWITCHMYLOANTkqRs&firstName='.$name[0].'&surName='.trim($lastname).'&mobileNo='.$result['phone_no'].'&email='.$result['email_id'].'&noValidationByPass=0&emailConditionalByPass=1';
     $apitype= "CUSTOM";
+
+    $curl = curl_init();
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 60);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        $response = curl_exec($curl);
+        $error = curl_error($curl);
+        $status = curl_getinfo($curl);
+        echo "Resp<br><br>";
+        print_r($response);
+        echo "Error<br><br>";
+        print_r($error);
+        echo "Status<br><br>";
+        print_r($status);
+        curl_close($curl);
+        return $response;
+
+
 }
 // else if($type == 2){
 //     $url = "https://ecvuat.experian.in/ECV-P2/content/validateMobileOTP.action";
 //     $apitype= "NORMAL";
 // }
- $response = curl_helper($url,$header,$content);
+//  $response = curl_helper($url,$header,$content);
 
- print_r($response);
+//  print_r($response);
 }
 
 echo '<script>window.location.href = "'.$head_url.'/query/edit.php?id='.$query_id.';</script>';
