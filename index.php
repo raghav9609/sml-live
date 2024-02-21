22cdaedac7353470d376e8cecb8e1fd473e3fc0f
Sumit Raghav <raghav9609@gmail.com>
2024-02-21 11:40:13 +0530

    changes

--- index.php
+++ index.php
@@ -14,16 +14,16 @@ require_once(dirname(__FILE__) . '/include/constant.php');
         <link rel="shortcut icon" href="<?php echo $head_url; ?>/assets/images/favicon.png" type="image/x-icon">
         <link rel="icon" href="<?php echo $head_url; ?>/assets/images/favicon.png" type="image/x-icon">
 		<?php
-		if (!(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' ||
-		$_SERVER['HTTPS'] == 1) ||
-		isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
-		$_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'))
-		{
-			$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-			header('HTTP/1.1 301 Moved Permanently');
-			header('Location: ' . $redirect);
-			exit();
-		}
+		// if (!(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' ||
+		// $_SERVER['HTTPS'] == 1) ||
+		// isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+		// $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'))
+		// {
+		// 	$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		// 	header('HTTP/1.1 301 Moved Permanently');
+		// 	header('Location: ' . $redirect);
+		// 	exit();
+		// }
 		?>
     </head>
     <body leftmargin="0" topmargin="0">
