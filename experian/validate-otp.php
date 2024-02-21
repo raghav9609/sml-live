f32f44faffd942f681686cdfa3562094c23c9e48
Sumit Raghav <raghav9609@gmail.com>
2024-02-21 19:04:57 +0530

    changes

--- experian/validate-otp.php
+++ experian/validate-otp.php
@@ -9,7 +9,22 @@ $stage_one_id = base64_decode($_REQUEST['stage_one_id']);
 $stage_two_id = base64_decode($_REQUEST['stage_two_id']);
 $otp = base64_decode($_REQUEST['otp']);
 if(in_array($type,array(1,2)) && $query_id > 0){
- print_r($_REQUEST);
+    $get_customerData = mysqli_query($Conn1,"select * from crm_query as qry INNER JOIN crm_customer as customer ON qry.crm_customer_id = customer.id where qry.id = ".$query_id);
+    $result = mysqli_fetch_array($get_customerData);
+    $header = array('content-type:application/x-www-form-urlencoded');
+    $apitype = 'NORMAL';
+    if($type == 1){
+        $apitype = 'CUSTOM';
+    }
+    $url_otp_gen = "https://ecvuat.experian.in/ECV-P2/content/validateMobileOTP.action";
+    $content_otp_gen = 'stgOneHitId='.$jsonDecodeResp['stgOneHitId'].'&stgTwoHitId='.$jsonDecodeResp['stgTwoHitId'].'&mobileNo='.$result['phone_no'].'&type='.$apitype.'&otp='.$otp;
+    $response_otpgeneration = curl_helper($url_otp_gen,$header,$content_otp_gen);
+    $resp_otp_gen  = json_decode($response_otpgeneration,true);
+    if($resp_otp_gen['showHtmlReportForCreditReport'] != "" && $resp_otp_gen['showHtmlReportForCreditReport'] != "null"  && $resp_otp_gen['showHtmlReportForCreditReport'] != null){
+        $resp_otp_gen['errorString'] = "Report Generated Successfully";
+        mysqli_query($Conn1,"Insert into crm_experian_data set query_id='".$query_id."',xml_report='".base64_encode($resp_otp_gen['showHtmlReportForCreditReport'])."'");
+    }
+    echo json_encode(array(""errorstring"=>,"stgOneHitId"=>$jsonDecodeResp['stgOneHitId'],"stgTwoHitId"=>$jsonDecodeResp['stgTwoHitId']));
 }
 
 ?>
