41aff17758e8df448b49b8e053ee841be9f50266
Sumit Raghav <raghav9609@gmail.com>
2024-02-21 17:05:18 +0530

    changes

--- experian/index.php
+++ experian/index.php
@@ -8,6 +8,7 @@ $type = ($_REQUEST['type']);
 
 print_r($_REQUEST);
 if(in_array($type,array(1,2)) && $query_id > 0){
+    echo "select * from crm_query as qry INNER JOIN crm_customer as customer ON qry.crm_customer_id = customer.id where qry.id = ".$query_id;
     $get_customerData = mysqli_query($Conn1,"select * from crm_query as qry INNER JOIN crm_customer as customer ON qry.crm_customer_id = customer.id where qry.id = ".$query_id);
     $result = mysqli_fetch_array($get_customerData);
 $header = array('content-type:application/x-www-form-urlencoded');
@@ -20,7 +21,7 @@ if($type == 1){
         $lastname = trim($result['name']);
     }
     $url = "https://ecvuat.experian.in/ECV-P2/content/registerEnhancedMatchMobileOTP.action";
-    $content = 'clientName=SWITCH_EM&allowInput=1&allowEdit=1&allowCaptcha=1&allowConsent=1&allowEmailVerify=1&allowVoucher=1&voucherCode=SWITCHMYLOANTkqRs&firstName='.$name[0].'&surName='.trim($lastname).'&mobileNo='.$result['phone_no'].'&email='.$result['email_id'].'&noValidationByPass=0&emailConditionalByPass=1';
+    echo $content = 'clientName=SWITCH_EM&allowInput=1&allowEdit=1&allowCaptcha=1&allowConsent=1&allowEmailVerify=1&allowVoucher=1&voucherCode=SWITCHMYLOANTkqRs&firstName='.$name[0].'&surName='.trim($lastname).'&mobileNo='.$result['phone_no'].'&email='.$result['email_id'].'&noValidationByPass=0&emailConditionalByPass=1';
     $apitype= "CUSTOM";
 }
 // else if($type == 2){
