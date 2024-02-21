bf833be09a430b03878370c4ca57bf035e0c58e7
Sumit Raghav <raghav9609@gmail.com>
2024-02-21 17:39:36 +0530

    changes

--- experian/validate-otp.php
+++ experian/validate-otp.php
@@ -7,8 +7,9 @@ $query_id = base64_decode($_REQUEST['query_id']);
 $type = ($_REQUEST['type']);
 $stage_one_id = base64_decode($_REQUEST['stage_one_id']);
 $stage_two_id = base64_decode($_REQUEST['stage_two_id']);
+$otp = base64_decode($_REQUEST['otp']);
 if(in_array($type,array(1,2)) && $query_id > 0){
- 
+ print_r($_REQUEST);
 }
 
 ?>
