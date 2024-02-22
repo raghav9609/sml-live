cca1bba6c615ef3f70f445355ff6fe2c8578821b
Sumit Raghav <raghav9609@gmail.com>
2024-02-22 16:04:10 +0530

    as

--- welcome.php
+++ welcome.php
@@ -42,6 +42,7 @@ $dispDateArr = array('Today','Yesterday','Last 7 Days','Last 30 Days');
                         $getreport = "SELECT usr.name As user_name,count(qry.id) As Total_count, stat.value As status FROM `crm_query` As qry left JOIN crm_master_user As usr ON qry.lead_assign_to = usr.id LEFT JOIN crm_master_status As stat ON qry.query_status = stat.id WHERE stat.status_type = 1 and qry.lead_assign_to = '".$user_id."' ";
 
                         foreach($dispDateArr As $dat){
+                            $getreportnew = '';
                             if($dat == 'Today'){
                                 $getreportnew = " and date(qry.created_on) = CURDATE() GROUP by qry.query_status ";
                             } else if($dat == 'Yesterday'){
