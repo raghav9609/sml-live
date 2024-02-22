a7866a656069fecee2cca190044729f3eed4f062
Sumit Raghav <raghav9609@gmail.com>
2024-02-22 11:50:31 +0530

    as

--- offers/index.php
+++ offers/index.php
@@ -22,10 +22,10 @@ if ($recordcount > 0) {
         $record++;
         $disablcls = '';
         $textclas = '';
-        if(in_array($exe_form['id'],$createdApplications)){
-            $disablcls = 'checked disabled';
-            $textclas = 'green bold';
-        }
+        // if(in_array($exe_form['id'],$createdApplications)){
+        //     $disablcls = 'checked disabled';
+        //     $textclas = 'green bold';
+        // }
         $data_bnk[] = 'anu';
         // $data_bnk[] = '<input type ="checkbox" style="position: unset !important;" class="check_bank" name = "check_bank[]" id = "check_bank_'.$exe_form['id'].'" value ="'.$exe_form['id'].'" '.$disablcls.'><label class="cursor '.$textclas.'" for="check_bank_'.$exe_form['id'].'">'.$exe_form['value'].'</label>&nbsp;&nbsp;&nbsp;&nbsp;';
     }
