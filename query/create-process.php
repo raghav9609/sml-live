86ebf3e97c7a744cf0e5dabb2915238a5efb730b
Sumit Raghav <raghav9609@gmail.com>
2024-02-22 18:56:32 +0530

    as

--- query/create-process.php
+++ query/create-process.php
@@ -14,6 +14,9 @@ $loanAmount = $_REQUEST['loan_amount'];
 if($user_role == 3){
    $user_id = $_SESSION['userDetails']['user_id']; 
 }
+
+echo "Select qry.net_income as income,qry.loan_amount, cust.* from crm_query As qry Inner JOIN crm_customer As cust qry.crm_customer_id = cust.id where qry.id = '".$query_id."'";
+
 $getcustdetails = mysqli_query($Conn1,"Select qry.net_income as income,qry.loan_amount, cust.* from crm_query As qry Inner JOIN crm_customer As cust qry.crm_customer_id = cust.id where qry.id = '".$query_id."'");
 $resultDetails = mysqli_fetch_array($getcustdetails);
 $city_name = '';
@@ -27,7 +30,7 @@ if($resultDetails['occupation_id'] > 0){
     $occup_namenm = get_name('master_code_id',$resultDetails['occupation_id']);
     $occup_name = $occup_namenm['value'];
 }
-
+echo "anu";
 $companyId = $resultDetails['company_id'];
 $modeSalary = $resultDetails['mode_of_salary'];
 $pincode = $resultDetails['pincode'];
@@ -35,6 +38,7 @@ $salary_bank_id = $resultDetails['salary_bank_id'];
 $ccweget = dateDiff($curDate,$result_cust_data['current_work_exp'],1);
 $tweget = dateDiff($curDate,$result_cust_data['total_work_exp'],1);
 $bnknm = '';
+echo "anu 1";
 if($salary_bank_id > 0){
     $bankfetch = get_name('master_code_id',$salary_bank_id);
     $bnknm = $bankfetch['value'];
@@ -51,7 +55,7 @@ if($modeSalary > 0){
     $salarymode = get_name('master_code_id',$modeSalary);
     $modesal = $salarymode['value'];
 }
-
+echo "anu 2";
 
 echo $maildata = '<table><tr><th>SML Lead ID</th><td>'".$query_id."'</td></tr>
         <tr><th>Customer</th><td>'".$resultDetails['name']."'<br>'".$resultDetails['phone_no']."'<br>'".$resultDetails['email']."'<br>'".$city_name."'<br>'".$resultDetails['dob']."'</td></tr><tr><th>Occupation</th><td>'".$occup_name."' @ '".$compnm."' NTH Rs. '".$resultDetails['income']."'<br> Paid By '".$modesal."' '".$bnknm."'<br> CWE '".$ccweget."' Months TWE '".$tweget."' Months</td></tr><tr><th>Loan Amount / Type</th><td>'".$resultDetails['loan_amount']."' Personal Loan</td></tr><tr><th>Residential Address</th><td>'".$resultDetails['address']."'</td></tr><tr><th>SML User</th><td>'".$_SESSION['userDetails']['user_name']."'</td></tr></table>';
