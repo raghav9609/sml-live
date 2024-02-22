2e4305a993d42bb21a2d20c2eb6d0aa1eda58519
Sumit Raghav <raghav9609@gmail.com>
2024-02-22 11:33:07 +0530

    as

--- offers/index.php
+++ offers/index.php
@@ -11,26 +11,26 @@ while($resultApp = mysqli_fetch_array($get_applicationcreated)){
     $createdApplications[] = $resultApp['bank_id'];
 }
 
-$getqrydetails =  mysqli_query($Conn1,"Select * from crm_query As qry Inner JOIN crm_customer As cust ON qry.crm_customer_id = qry.id where id = '".$query_id."'");
-$resqrydets = mysqli_fetch_array($getqrydetails);
+// $getqrydetails =  mysqli_query($Conn1,"Select * from crm_query As qry Inner JOIN crm_customer As cust ON qry.crm_customer_id = qry.id where id = '".$query_id."'");
+// $resqrydets = mysqli_fetch_array($getqrydetails);
 
-$companyId = $resqrydets['company_id'];
-$modeSalary = $resqrydets['mode_of_salary'];
+// $companyId = $resqrydets['company_id'];
+// $modeSalary = $resqrydets['mode_of_salary'];
 
-if($companyId > 0){
-    $compnmfetch = get_name('comp_name',$companyId);
-    $compnm = $compnmfetch['company_name'];
-} else {
-    $compnm = $resqrydets['company_name'];
-}
-$modesal = 'Bank Transfer';
-if($modeSalary > 0){
-    $salarymode = get_name('master_code_id',$modeSalary);
-    $modesal = $salarymode['value'];
-    if($modeSalary == 6){
-        $modesal = 'Bank Transfer';
-    }
-}
+// if($companyId > 0){
+//     $compnmfetch = get_name('comp_name',$companyId);
+//     $compnm = $compnmfetch['company_name'];
+// } else {
+//     $compnm = $resqrydets['company_name'];
+// }
+// $modesal = 'Bank Transfer';
+// if($modeSalary > 0){
+//     $salarymode = get_name('master_code_id',$modeSalary);
+//     $modesal = $salarymode['value'];
+//     if($modeSalary == 6){
+//         $modesal = 'Bank Transfer';
+//     }
+// }
 
 //print_r($createdApplications);
 $qry1 = "select * from crm_masters where crm_masters_code_id = 10 and is_active = 1 ";
