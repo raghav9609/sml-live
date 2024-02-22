8152f14fc1bccbd5c8c443787b236cb42a0d1128
Sumit Raghav <raghav9609@gmail.com>
2024-02-22 12:06:43 +0530

    as

--- include/case-query-function-insert.php
+++ include/case-query-function-insert.php
@@ -51,15 +51,16 @@
             data: "query_id="+query_id+"&cust_iddd="+cust_iddd,
             success: function(response) {
                 $("#new_offers_journey").html(response);
-                
+                if(get_val == 2){
                     $('#form_step3').prev('.brdr-top-gray').addClass('step-green').removeClass('blue-bg');
                     $('#form_step1').slideUp(300);
                     $('#form_step3').slideDown(300);
-                
-                // $('#form_1').prev('.brdr-top-gray').addClass('step-green').removeClass('blue-bg');
-                // $('#form_1').next('.blue-bg').addClass('white').removeClass('gray');
-                // $('#form_1').next('.brdr-top-gray').next('form').slideDown(300);
-                // $('#form_1').slideUp(300);
+                } else {
+                    $('#form_step1').prev('.brdr-top-gray').addClass('step-green').removeClass('blue-bg');
+                    $('#form_step1').next('.blue-bg').addClass('white').removeClass('gray');
+                    $('#form_step1').next('.brdr-top-gray').next('form').slideDown(300);
+                    $('#form_step1').slideUp(300);
+                }
             }
         });
         //Changes - GoldLoanApi - Akash - Ends
