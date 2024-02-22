1f0cd060499c8096ad3142a6459ba0bdcf4f45bd
Sumit Raghav <raghav9609@gmail.com>
2024-02-22 13:43:48 +0530

    changes

--- sms/send-sms-process.php
+++ sms/send-sms-process.php
@@ -8,23 +8,16 @@ $subject = $_REQUEST['subject'];
 $description = str_replace(array("\r", "\n"), '', $_REQUEST['description']);
 $temp_id = $_REQUEST['template'];											
 $query_id = $_REQUEST['query_id']; 
-// echo $url= "https://bulkpush.mytoday.com/BulkSms/SingleMsgApi?";
-// $response = curl_helper($url);
-
-
 
 $header = array('content-type:application/x-www-form-urlencoded');
 $url = "https://bulkpush.mytoday.com/BulkSms/SingleMsgApi";
-echo $content = "async=1&feedid=385302&username=9920706289&password=SML2021@123&senderid=SML&To=91".$subject."&Text=".str_replace(' ','+',$description);
-echo $response = curl_helper($url,$header,$content);
+$content = "async=1&feedid=385302&username=9920706289&password=SML2021@123&senderid=SML&To=91".$subject."&Text=".str_replace(' ','+',$description);
+$response = curl_helper($url,$header,$content);
 
-
-
-print_r($response);
-// $qry = "INSERT INTO crm_communication_history SET query_id='".$query_id."',type = 2, communication_id = '".$subject."',response='".base64_encode($response)."',description = '".base64_encode($description)."'";
-// $insert_comm = mysqli_query($Conn1,$qry);
-//echo '<script>window.location.href = "'.$head_url.'/query/";</script>';
-        ?>       
+$qry = "INSERT INTO crm_communication_history SET query_id='".$query_id."',type = 2, communication_id = '".$subject."',response='".base64_encode($response)."',description = '".base64_encode($description)."'";
+$insert_comm = mysqli_query($Conn1,$qry);
+echo '<script>window.location.href = "'.$head_url.'/query/";</script>';
+?>       
  
         
                                          
