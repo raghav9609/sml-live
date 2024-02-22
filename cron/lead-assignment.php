c66121fa0b2655c51cba5c5d43a5a339438950ab
Sumit Raghav <raghav9609@gmail.com>
2024-02-22 09:32:02 +0530

    as

--- cron/lead-assignment.php
+++ cron/lead-assignment.php
@@ -21,6 +21,7 @@ foreach($fetch_unassign_leads as $key=>$value){
     }
 
     $data = array("city_sub_group_id"=>$city_sub_group_id,"net_income"=>$final_net_income,"loan_amount"=>$loan_amount,"loan_type_id"=>$loan_type_id);
+    echo "<br>".$leadAssignmentClassexport->leadAssignment($data);
     $getLeadAssignment = $db_handle->runQuery($leadAssignmentClassexport->leadAssignment($data));
     $data_to_update = array("lead_assign_to"=>$getLeadAssignment[0]['shift1user_id'],"query_id"=>$query_id);
     $updateUserIdQuery = $db_handle->updateRows($leadAssignmentClassexport->userQueryID($data_to_update));
