206349d73b21f25d29e7ee2665617dc042f345d3
Sumit Raghav <raghav9609@gmail.com>
2024-02-22 11:45:53 +0530

    as

--- offers/index.php
+++ offers/index.php
@@ -26,7 +26,7 @@ if ($recordcount > 0) {
             $textclas = 'green bold';
         }
         $data_bnk[] = '<input type ="checkbox" style="position: unset !important;" class="check_bank" name = "check_bank[]" id = "check_bank_'.$exe_form['id'].'" value ="'.$exe_form['id'].'" ><label class="cursor '.$textclas.'" for="check_bank_'.$exe_form['id'].'">'.$exe_form['value'].'</label>&nbsp;&nbsp;&nbsp;&nbsp;';
-        print_r($data_bnk);
+       // print_r($data_bnk);
     }
     print_r($data_bnk);
     echo implode($data_bnk);
