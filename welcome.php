a9c00f3d8fd9adc5f8491cf7bd30f612b3ce04c3
Sumit Raghav <raghav9609@gmail.com>
2024-02-22 16:07:20 +0530

    as

--- welcome.php
+++ welcome.php
@@ -54,7 +54,7 @@ $dispDateArr = array('Today','Yesterday','Last 7 Days','Last 30 Days');
                                 $getreportnew = " and date(qry.created_on) = date_sub(CURDATE(),interval 30 day) GROUP by qry.query_status ";
                             }
                             echo $getreportcreated = $getreport.$getreportnew;
-                            $resreport = mysqli_query($Conn1,$getreport);
+                            $resreport = mysqli_query($Conn1,$getreportcreated);
                             while($resdata = mysqli_fetch_array($resreport)){
                                 $datadisp[$dat][$resdata['status']] = $resdata['Total_count'];
                             }
