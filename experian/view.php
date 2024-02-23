3e6424a3235b95dccc37bfb643698cd764dc67f2
Sumit Raghav <raghav9609@gmail.com>
2024-02-23 12:39:08 +0530

    as

--- experian/view.php
+++ experian/view.php
@@ -3,6 +3,10 @@
 require_once(dirname(__FILE__) . '/../config/session.php');
 require_once(dirname(__FILE__) . '/../config/config.php');
 require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
+error_reporting(E_ALL); 
+ini_set('display_errors', true); 
+libxml_use_internal_errors(false);
+
 $query_id = base64_decode($_REQUEST['query_id']);
 $getbureaudetails = mysqli_query($Conn1,"Select * from crm_experian_data where query_id = '".$query_id."'");
     $resultbureaudetails = mysqli_fetch_array($getbureaudetails);
