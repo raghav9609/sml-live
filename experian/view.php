49de0fda6d9c7c7d3679f9e379a9bcd074ec1930
Sumit Raghav <raghav9609@gmail.com>
2024-02-23 12:26:30 +0530

    as

--- experian/view.php
+++ experian/view.php
@@ -9,7 +9,7 @@ $getbureaudetails = mysqli_query($Conn1,"Select * from crm_experian_data where q
     $bureauData = $resultbureaudetails['xml_report'];
     if($bureauData != ''){
         $dispBureauData = base64_decode($bureauData);
-        echo gettype($dispBureauData);
+        echo gettype(html_entity_decode($dispBureauData));
         // $dispBureauData = preg_replace("/(<\/?)(\w+):([^>]*>)/", '$1$2$3', $dispBureauData);
         // $json_response = str_replace("ns0:","",$dispBureauData);
         // $xml = new SimpleXMLElement($json_response);
