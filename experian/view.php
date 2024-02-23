9ef8ed62d3b2e782e7e65d35e1bbab3300904e1f
Sumit Raghav <raghav9609@gmail.com>
2024-02-23 21:46:49 +0530

    changes

--- experian/view.php
+++ experian/view.php
@@ -140,7 +140,21 @@ $getbureaudetails = mysqli_query($Conn1,"Select * from crm_experian_data where q
                         
                     $i =0;$data=array();
                         foreach($returnResponse['CAIS_Account']['CAIS_Account_DETAILS'] as $key => $result_fetch_acc_details){
-							
+							$i++;
+                            $data[] = $result_fetch_acc_details;
+                            if($result_fetch_acc_details['Open_Date'] != '1970-01-01' && $result_fetch_acc_details['Open_Date'] != '0000-00-00' && $result_fetch_acc_details['Open_Date'] !='' ){
+                                $open_date_to_display = date('d-m-Y',strtotime($result_fetch_acc_details['Open_Date']));
+                            }else{
+                                $open_date_to_display = '';
+                            }
+							echo $i;
+							preArray($result_fetch_acc_details);
+                    echo $template .= '<tr>
+                            <td style="border-right: 1px solid #d6d6d6;border-top:1px solid #d6d6d6;font-weight: bold;color: #008db1;padding: 5px;">Acct '.$i.'</td>
+                            <td style="border-right: 1px solid #d6d6d6;border-top:1px solid #d6d6d6;padding: 5px;">'.$result_fetch_acc_details['Subscriber_Name'].'</td>
+                            <td style="border-right: 1px solid #d6d6d6;border-top:1px solid #d6d6d6;padding: 5px;">'.$result_fetch_acc_details['Account_Type'].'</td>
+                            
+                        </tr>';
                         }
                   $template .= '</table>
                 </td>
