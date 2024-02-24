10d65806ad733aadf299b013a8aa987b9a403bff
Sumit Raghav <raghav9609@gmail.com>
2024-02-24 08:30:04 +0530

    changes

--- experian/view.php
+++ experian/view.php
@@ -389,23 +389,9 @@ $getbureaudetails = mysqli_query($Conn1,"Select * from crm_experian_data where q
 							print_r($val_y);
                         $template .= '<tr>
                             <td style="font-weight: bold;color: #008db1;border-right: 1px solid #ffffff;border-bottom: 1px solid #ffffff;background: #dadada;padding: 5px">'.$val_y['Year'].'</td>';
-                        for($i=12;$i>=1;$i--){
-							if($i == $val_y['Month']){
-								if($val_y['Days_Past_Due'] == ''){
-									$style='border-right: 1px solid #d6d6d6;border-bottom: 1px solid #d6d6d6;padding: 5px';
-								}else if($val_y['Days_Past_Due'] == '0'){
-								   $style='font-weight: bold;color: #ffffff;border-right: 1px solid #d6d6d6;border-bottom: 1px solid #d6d6d6;padding: 5px;background: #43ad43';
-								}else if($val_y['Days_Past_Due'] >= '90'){
-								 $style='font-weight: bold;color: #ffffff;border-right: 1px solid #d6d6d6;border-bottom: 1px solid #d6d6d6;padding: 5px;background: red';
-								}else{
-								 $style='font-weight: bold;color: #ffffff;border-right: 1px solid #d6d6d6;border-bottom: 1px solid #d6d6d6;padding: 5px;background: #f6650b';
-								}
-								$template .= '<td style="'.$style.'">'.$val_y['Days_Past_Due'].'</td>';
-							}else{
-								$template .= '<td style="'.$style.'"></td>';
-							}	
-                        }
-                    $template .= '</tr>';}
+                        
+                    $template .= '</tr>';
+				}
                  $template .= '</table>
                 </td>
             </tr>
