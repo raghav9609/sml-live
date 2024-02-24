c3ffa8c8ae8150788c17966fface9c13fabcc9b6
Sumit Raghav <raghav9609@gmail.com>
2024-02-24 11:56:40 +0530

    changes

--- experian/view.php
+++ experian/view.php
@@ -383,7 +383,6 @@ $getbureaudetails = mysqli_query($Conn1,"Select * from crm_experian_data where q
                             <td style="font-weight: bold;color: #008db1;border-right: 1px solid #ffffff;border-bottom: 1px solid #ffffff;background: #dadada;padding: 5px">Feb</td>
                             <td style="font-weight: bold;color: #008db1;border-right: 1px solid #ffffff;border-bottom: 1px solid #ffffff;background: #dadada;padding: 5pxbackground: #dadada;padding: 5px">Jan</td>
                         </tr>';
-						preArray($val['CAIS_Account_History']);
 						$final_dpd_array = array();
 						// foreach($val['CAIS_Account_History'] as $key_yn=>$val_yn){
 						// 	$final_dpd_array[$val_yn['Year']][$val_yn['Month']] = $val_yn['Days_Past_Due'];
