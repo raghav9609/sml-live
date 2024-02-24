e013c9f4266003d3b264a0d8458370036dd3f9f2
Sumit Raghav <raghav9609@gmail.com>
2024-02-24 11:30:53 +0530

    chages

--- experian/view.php
+++ experian/view.php
@@ -388,7 +388,7 @@ $getbureaudetails = mysqli_query($Conn1,"Select * from crm_experian_data where q
 							$final_dpd_array[$val_yn['Year']][] = array($val_yn['Month']=>$val_yn['Days_Past_Due']);
 						}
 
-						preArray($final_dpd_array);
+						//preArray($final_dpd_array);
 						
                         foreach($final_dpd_array as $key=>$val_y){
 							//echo $key;
