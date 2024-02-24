be11a0ebe155f72f7acd7cb273cfe36089273297
Sumit Raghav <raghav9609@gmail.com>
2024-02-24 08:45:59 +0530

    changes

--- experian/view.php
+++ experian/view.php
@@ -391,6 +391,7 @@ $getbureaudetails = mysqli_query($Conn1,"Select * from crm_experian_data where q
 						//preArray($final_dpd_array);
 						
                         foreach($final_dpd_array as $key=>$val_y){
+							echo $key;
                         $template .= '<tr>
                             <td style="font-weight: bold;color: #008db1;border-right: 1px solid #ffffff;border-bottom: 1px solid #ffffff;background: #dadada;padding: 5px">'.$key.'</td>';
                         
