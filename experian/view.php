ca91510f2f48ad099d9203933a0f832e24f08918
Sumit Raghav <raghav9609@gmail.com>
2024-02-24 11:46:20 +0530

    changes

--- experian/view.php
+++ experian/view.php
@@ -388,7 +388,7 @@ $getbureaudetails = mysqli_query($Conn1,"Select * from crm_experian_data where q
 							$final_dpd_array[$val_yn['Year']][$val_yn['Month']] = $val_yn['Days_Past_Due'];
 						}
 
-						var_dump($final_dpd_array);
+						print_r($final_dpd_array);
 						
                         foreach($final_dpd_array as $key_valueee => $val_y){
 							//echo $key;
