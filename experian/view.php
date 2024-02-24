adcf44af17efcf9e9339dfb6d06a4e3086bc10e1
Sumit Raghav <raghav9609@gmail.com>
2024-02-24 08:32:39 +0530

    changes

--- experian/view.php
+++ experian/view.php
@@ -386,7 +386,6 @@ $getbureaudetails = mysqli_query($Conn1,"Select * from crm_experian_data where q
 
 						
                         foreach($val['CAIS_Account_History'] as $key_y=>$val_y){
-							print_r($val_y);
                         $template .= '<tr>
                             <td style="font-weight: bold;color: #008db1;border-right: 1px solid #ffffff;border-bottom: 1px solid #ffffff;background: #dadada;padding: 5px"></td>';
                         for($i=12;$i>=1;$i--){
