2a53c5c135337757c2dc46484c634dd373995118
Sumit Raghav <raghav9609@gmail.com>
2024-02-28 13:15:21 +0530

    as

--- query/create-process.php
+++ query/create-process.php
@@ -9,12 +9,11 @@ require_once(dirname(__FILE__) . '/../include/class.mailer.php');
 $partnerId = $_REQUEST['partner_id'];
 $query_id = $_REQUEST['query_id'];
 $explpat = explode(',',$partnerId);
-$user_id = $_REQUEST['user_id'];
+//$user_id = $_REQUEST['user_id'];
 $loanAmount = $_REQUEST['loan_amount'];
 
-if($user_role == 3){
    $user_id = $_SESSION['userDetails']['user_id']; 
-}
+
 
 $getcustdetails = mysqli_query($Conn1,"Select qry.net_income as income,qry.loan_amount, cust.* from crm_query As qry Inner JOIN crm_customer As cust ON qry.crm_customer_id = cust.id where qry.id = '".$query_id."'");
 $resultDetails = mysqli_fetch_array($getcustdetails);
